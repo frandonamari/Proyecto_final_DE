066ba1fff802e0bbbe2e1419d1df38267d6a931e
Francis Donamari <fran.d.donamar@gmail.com>
2023-12-17 19:41:34 -0300

    new file:   Proyecto_final/.gitignore 	new file:   Proyecto_final/Readme.md 	new file:   Proyecto_final/dags/dag_spotify.py 	new file:   Proyecto_final/docker-compose.yaml 	new file:   Proyecto_final/requirements/requirements.txt 	new file:   Proyecto_final/scripts/entregafinalprueba.py 	new file:   Proyecto_final/scripts/utils.py 	new file:   Proyecto_final/sql/create_table.sql

--- Proyecto_final/dags/dag_spotify.py
+++ Proyecto_final/dags/dag_spotify.py
@@ -1,9 +1,30 @@
 from datetime import datetime, timedelta
 import sqlalchemy as sa
-from airflow import DAG
+from airflow.models import DAG, Variable
 from airflow.operators.python_operator import PythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from scripts.entregafinalprueba import create_df 
+
+import smtplib
+
+def enviar(context):
+    try:
+        x=smtplib.SMTP('smtp.gmail.com',587)
+        x.starttls()
+        x.login('fran.d.donamari@gmail.com',
+                Variable.get('GMAIL_SECRET')
+        
+        )        
+
+        subject=f'Reporte de Airflow{context["dag"]} {context['ds']}'
+        body_text=f'Tarea {context['task_instance_key_str']}se ejecuto correctamente'
+        message='Subject: {}\n\n{}'.format(subject,body_text)
+        x.sendmail('fran.d.donamari@gmail.com','fran.d.donamri@gmail.com',message)
+        print('Exito')
+    except Exception as exception:
+        print(exception)
+        print('Failure')
+
 default_args = {
     "owner":"Frandonamari",
     "description":"Api que toma el top 50 de canciones globales en Spotify",
@@ -13,12 +34,14 @@ default_args = {
     "retry_delay": timedelta(minutes=1)
     }
 
+
 with DAG(
     dag_id="dag_spotify_catchup",
     start_date=datetime(2023, 12, 7),
     catchup=True,
     schedule_interval= "@daily",
-    default_args=default_args
+    default_args=default_args,
+    
 ) as dag:
     
     create_df_task = PythonOperator(
@@ -27,5 +50,7 @@ with DAG(
         op_kwargs={
             "url_play": "https://accounts.spotify.com/api/token",
 
-        }
+        },
+        on_success_callback= enviar
+        
     )
