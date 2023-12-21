ae89dac34a721bdbf7faa49f32bb755c7461f19a
Francis Donamari <fran.d.donamar@gmail.com>
2023-12-20 22:27:16 -0300

    Proyecto terminado

--- Proyecto_final/dags/dag_spotify.py
+++ Proyecto_final/dags/dag_spotify.py
@@ -4,50 +4,91 @@ from airflow.models import DAG, Variable
 from email import message
 from airflow.operators.python_operator import PythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
-from scripts.entregafinalprueba import create_df 
-
+from scripts.entregafinalprueba import create_df , df_spotify
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import smtplib
 
-def enviar(context):
+
+def enviar_informacion_por_correo(**kwargs):
     try:
+       # Obtener el DataFrame del contexto
+        ti = kwargs['ti']
+        df_spotify = ti.xcom_pull(task_ids='create_df', key='return_value')
+
+        # Mensajes de depuración
+        print("DataFrame recibido:")
+        print(df_spotify.head())  # Imprimir las primeras filas del DataFrame
+
+        # Verificar que 'artist_name' está presente en el DataFrame
+        if 'artist_name' not in df_spotify.columns:
+            raise ValueError("La columna 'artist_name' no está presente en el DataFrame.")
+
+        resumen = df_spotify.groupby('artist_name').size().reset_index(name='cantidad')
+
+         # Mensajes de depuración
+        print("Resumen:")
+        print(resumen.head())
+
+       # Crear el mensaje del correo electrónico
+        mensaje = MIMEMultipart()
+        mensaje['From'] = "fran.d.donamari@gmail.com"
+        mensaje['To'] = "fran.d.donamari@gmail.com"
+        mensaje['Subject'] = 'Resumen de Cantidad por Artista'
+
+      # Agregar el cuerpo del mensaje con el resumen de la cantidad por artista
+        cuerpo_mensaje = MIMEText(resumen.to_html(index=False), 'html', 'utf-8')  # Asegúrate de que no incluya el índice
+        cuerpo_mensaje['Content-Transfer-Encoding'] = 'quoted-printable'
+        mensaje.attach(cuerpo_mensaje)
+
         x=smtplib.SMTP('smtp.gmail.com',587)
         x.starttls()
         x.login('fran.d.donamari@gmail.com',
-                Variable.get('GMAIL_SECRET')
+                 Variable.get('GMAIL_SECRET')
         
-        )        
+         )    
 
-        subject=f'Reporte de Airflow create_df_task'
-        body_text='Tarea se ejecuto correctamente'
-        message='Subject: {}\n\n{}'.format(subject,body_text)
-        x.sendmail('fran.d.donamari@gmail.com','fran.d.donamri@gmail.com',message)
+        x.sendmail("fran.d.donamari@gmail.com", "fran.d.donamari@gmail.com", mensaje.as_string())
+
+        # Cerrar la conexión
+        x.quit()
+
         print('Exito')
+
     except Exception as exception:
         print(exception)
         print('Failure')
 
+
 default_args = {
-    "owner":"Frandonamari",
-    "description":"Api que toma el top 50 de canciones globales en Spotify",
-    "depend_on_past":False,
+    "owner": "Frandonamari",
+    "description": "Api que toma el top 50 de canciones globales en Spotify",
+    "depend_on_past": False,
     "email": ['fran.d.donamari@gmail.com'],
     "retries": 3,
     "retry_delay": timedelta(minutes=1)
-    }
-
+}
 
 with DAG(
     dag_id="dag_spotify_catchup",
     start_date=datetime(2023, 12, 7),
     catchup=True,
-    schedule_interval= "@daily",
+    schedule_interval="@daily",
     default_args=default_args,
-    
 ) as dag:
     
     create_df_task = PythonOperator(
         task_id="create_df",
         python_callable=create_df,
-        on_success_callback= enviar,     
-        
-)
+        dag=dag,
+        provide_context=True
+    )
+ 
+    send_emailresume_task = PythonOperator(
+        task_id="send_emailresume_task",
+        python_callable=enviar_informacion_por_correo,
+        dag=dag,
+        provide_context=True
+    )
+
+create_df_task >> send_emailresume_task
